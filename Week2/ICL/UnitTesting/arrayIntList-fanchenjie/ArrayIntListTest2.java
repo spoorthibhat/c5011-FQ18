9846a6f34557b1184e54c5b0c42beb2c7c4790d0
fanchenjie <43649719+fanchenjie@users.noreply.github.com>
2018-10-04 18:04:46 -0700

    Add files via upload

---
+++ Week2/ICL/UnitTesting/arrayIntList-fanchenjie/ArrayIntListTest2.java
@@ -0,0 +1,39 @@
+package arrayIntList;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ArrayIntListTest2 {
+
+   @Before
+   public void setUp() throws Exception {
+   }
+
+   @Test
+   public void testGet() {
+      fail("Not yet implemented");
+   }
+
+   @Test
+   public void testSet() {
+      fail("Not yet implemented");
+   }
+
+   @Test
+   public void testSize() {
+      fail("Not yet implemented");
+   }
+
+   @Test
+   public void testIsEmpty() {
+      fail("Not yet implemented");
+   }
+
+   @Test
+   public void testRemove() {
+      fail("Not yet implemented");
+   }
+
+}
